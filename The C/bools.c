19f0e639d529cbefc35e8ef3d3e101ec152e7c64
Squirrel <squirrelhomebrew@gmail.com>
2025-01-21 10:31:24 -0800

    Create bools.c

---
+++ The C/bools.c
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+
+int main() {
+    bool doesTrumpSuckAss = true;
+
+    printf("%d\n", doesTrumpSuckAss);
+
+    printf("Btw, 1 equals true.. Trump needs to fucking die.");
+    
+    return 0;
+}
