c48cd64ce6737bd400a4033a843bedf1f2f5b69b
Squirrel <squirrelhomebrew@gmail.com>
2025-01-21 12:03:12 -0800

    Create greg cases.c

---
+++ The C/greg cases.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main() {
+    int gregPeakLevel = 1;
+
+
+    switch (gregPeakLevel) {
+        case 1:
+            printf("You are level 1 peak");
+            break;
+        case 2:
+            printf("you are level 2 peak");
+            break;
+        case 3
+            printf("you are level 3 peak");
+            break;
+    }
+}
