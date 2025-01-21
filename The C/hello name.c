1999df17904bf328b5defe9adf633927b8646d32
Squirrel <squirrelhomebrew@gmail.com>
2025-01-21 14:05:46 -0800

    I hope trump fucking dies :3

--- The C/hello name.c
+++ The C/hello name.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
   printf("what is name?\n");
   char name[60];
 
   scanf("%s", name);
-
-  printf("hai %s!!!", name);
+  
+  if (strcmp(name, "trump") == 0) {
+      printf("Kys.");
+  } else {
+      printf("hai %s!!!", name);
+  }
 }
