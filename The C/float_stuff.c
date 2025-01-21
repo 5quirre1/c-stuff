0fc453f76083f06a1cdbe97ac7e72d829d83e0b5
Squirrel <squirrelhomebrew@gmail.com>
2025-01-21 10:08:18 -0800

    Create float_stuff.c

---
+++ The C/float_stuff.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    float myFloatNum = 3.55;
+    printf("%f\n", myFloatNum);
+    printf("%.1f\n", myFloatNum);
+    printf("%.2f\n", myFloatNum);
+    printf("%.4f", myFloatNum);
+}
